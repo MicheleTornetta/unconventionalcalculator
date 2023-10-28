63250cac1fb74ba3f959a130feb6b30feb91db84
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-10-28 10:49:41 -0400

    object function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 //to get user input
 
@@ -10,7 +11,23 @@ function getUserImput(){
 function createWriteOutput(operator, resultBeforeCalculation, calcNumber) {
     //to run the current result before it's changed
     const calcDescription = `${resultBeforeCalculation} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); //from vendor file 
+}
+
+function writeToLog(
+    operationIdentifier, 
+    prevResult, 
+    keyedNumber, 
+    newResult){
+        const logEntry = {
+            operation: operationIdentifier,
+            prevResult: prevResult,
+            number: keyedNumber,
+            result: newResult
+        };
+        logEntries.push(logEntry);
+        console.log(logEntry.operation);
+        console.log(logEntries);
 }
 
 //for the calculator addition - see vendor.js for userInput
@@ -19,32 +36,36 @@ function addition() {
     const initialResult = currentResult;
     //to run the current result before it's changed
     // const calcDescription = `${currentResult} + ${keyedNumber}`;
-    currentResult = currentResult + keyedNumber;
+    currentResult += keyedNumber;
     // or     currentResult = currentResult + +userInput.value;
     //from Vendor.js file line 10 - use outputResult
     // outputResult(currentResult, calcDescription);
     createWriteOutput('+', initialResult, keyedNumber);
+    writeToLog('ADD', initialResult, keyedNumber, currentResult);
 }
 
 function subtract() {
     const keyedNumber = getUserImput();
     const initialResult = currentResult;
-    currentResult = currentResult - keyedNumber;
+    currentResult -= keyedNumber;
     createWriteOutput('-', initialResult, keyedNumber);
+    writeToLog('SUBTRACT', initialResult, keyedNumber, currentResult);
 }
 
 function multiply() {
     const keyedNumber = getUserImput();
     const initialResult = currentResult;
-    currentResult = currentResult * keyedNumber;
+    currentResult *= keyedNumber;
     createWriteOutput('*', initialResult, keyedNumber);
+    writeToLog('MULTIPLY', initialResult, keyedNumber, currentResult);
 }
 
 function divide() {
     const keyedNumber = getUserImput();
     const initialResult = currentResult;
-    currentResult = currentResult / keyedNumber;
+    currentResult /= keyedNumber;
     createWriteOutput('/', initialResult, keyedNumber);
+    writeToLog('DIVIDE', initialResult, keyedNumber, currentResult);
 }
 
 addBtn.addEventListener('click', addition);
